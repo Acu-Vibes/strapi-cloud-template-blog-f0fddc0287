1a6d5f161b85d24beff1daf55ce54150f3313f9c
humaid-valus <humaid.okasha@valus.io>
2025-10-17 13:12:23 +0500

    commit

--- src/api/global-setting/controllers/global-setting.js
+++ src/api/global-setting/controllers/global-setting.js
@@ -6,35 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::global-setting.global-setting');
-
-module.exports = createCoreController('api::global-setting.global-setting', ({ strapi }) => ({
-  async find(ctx) {
-    // Explicitly populate all fields including nested relations
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        points: {
-          populate: {
-            image: {
-              fields: ['url', 'alternativeText', 'name', 'width', 'height', 'formats'],
-            },
-          },
-        },
-        // Add ALL other fields from your global-setting content type that need population
-        // Examples (replace with your actual field names):
-        logo: {
-          fields: ['url', 'alternativeText', 'name', 'width', 'height', 'formats'],
-        },
-        banner: {
-          fields: ['url', 'alternativeText', 'name', 'width', 'height', 'formats'],
-        },
-        // Add other relations/components as needed
-      },
-    };
-
-    const { data, meta } = await super.find(ctx);
-
-    return { data, meta };
-  },
-}));
+module.exports = createCoreController('api::global-setting.global-setting');
